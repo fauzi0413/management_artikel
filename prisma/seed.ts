0bdcb559d670756a4f64cd1839138220e0cf1594
MIvanRais <26ivanrais@gmail.com>
2026-06-19 16:26:54 +0700

    Fix seed by adding slug and published fields

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcrypt";
+import slugify from "slugify";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -12,41 +13,48 @@ const prisma = new PrismaClient({
 });
 
 async function main() {
-    const hashedPassword = await bcrypt.hash(
-        "123456",
-        10
-    );
+  const hashedPassword = await bcrypt.hash("123456", 10);
 
-    await prisma.user.create({
-        data: {
-            name: "Admin",
-            email: "admin@example.com",
-            role: "admin",
-            password: hashedPassword,
-        },
-    });
+  await prisma.user.create({
+    data: {
+      name: "Admin",
+      email: "admin@example.com",
+      role: "admin",
+      password: hashedPassword,
+    },
+  });
 
-    const user = await prisma.user.create({
-        data: {
-            name: "User",
-            email: "user@example.com",
-            role: "user",
-            password: hashedPassword,
-        },
-    });
+  const user = await prisma.user.create({
+    data: {
+      name: "User",
+      email: "user@example.com",
+      role: "user",
+      password: hashedPassword,
+    },
+  });
 
-    const articles = Array.from({ length: 25 }, (_, i) => ({
-        title: `Artikel ${i + 1}`,
-        content: `
-        Artikel ke-${i + 1} membahas topik pengembangan aplikasi menggunakan Next.js,
-        Prisma, PostgreSQL, TypeScript, dan praktik terbaik dalam membangun aplikasi fullstack modern.
-        `.trim(),
-        userId: user.id,
-    }));
+  const articles = Array.from({ length: 25 }, (_, i) => {
+    const title = `Artikel ${i + 1}`;
 
-    await prisma.post.createMany({
-        data: articles,
-    });
+    return {
+      title,
+      slug: slugify(title, {
+        lower: true,
+        strict: true,
+        trim: true,
+      }),
+      content: `
+Artikel ke-${i + 1} membahas topik pengembangan aplikasi menggunakan Next.js,
+Prisma, PostgreSQL, TypeScript, dan praktik terbaik dalam membangun aplikasi fullstack modern.
+      `.trim(),
+      published: true,
+      userId: user.id,
+    };
+  });
+
+  await prisma.post.createMany({
+    data: articles,
+  });
 
   console.log("Seed berhasil");
 }
